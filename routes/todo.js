1b4e334b5f545897ce8ee09d07a63b123c90633b
june <junearagoncillo@gmail.com>
2020-12-03 10:45:22 +0800

    final

--- routes/todo.js
+++ routes/todo.js
@@ -36,20 +36,76 @@ router.get('/myTodos', verify, async (req, res) => {
 
 })
 
-// router.get('/todo/:id', verify, async (req, res) => { 
+router.get('/todo/:id', verify, async (req, res) => { 
 
-//     todo = await Todo.find({ _id: req.params.id })
+    todo = await Todo.find(
+        {
+            _id: req.params.id
+        })
 
-//     if (!todo) {
-//         return res.status(200).send('No todos found')
-//     }
-//     // else if (todo.user_id != req.user._id ) { 
-//     //     return res.status(200).send('this todo doesnt belongs to you')
-//     // }
-//     else { 
-//         return res.status(200).send(todo)
-//     }
+    if (!todo) {
+        return res.status(200).send('No todos found')
+    }
+    else if (todo[0].user_id != req.user._id ) { 
+        return res.status(200).send('this todo doesnt belongs to you')
+    }
+    else { 
+        return res.status(200).send(todo)
+    }
 
-// })
+})
+
+
+router.get('/todos', verify, async (req, res) => { 
+    
+    todo = await Todo.find()
+
+    if (!todo) {
+        return res.status(200).send('No Todos')
+    }
+    else { 
+        return res.status(200).send(todo)
+    }
+
+})
+
+router.delete('/deleteTodo/:id', verify, async (req, res) => {
+    
+    try { 
+
+        const removePost = await Todo.remove({ _id: req.params.id })
+        return res.status(200).json({
+            message: 'Todo deleted successfully'
+        })
+
+    } catch (err) {
+        
+        res.status(500).json({ error: err })
+    
+    }
+        
+})
+
+router.put('/updateTodo/:id', verify, async (req, res) => { 
+
+    const validTodo = await Todo.find({ _id: req.params.id })
+    
+    if (validTodo[0].user_id != req.user._id) { 
+        return res.status(201).json({message: 'This todo is not yours'})
+    }
+    
+    const newValues = { $set: { todo: req.body.todo }}
+
+    try {
+        const updatedTodo = await Todo.updateOne({ _id: req.params.id }, newValues)
+
+        return res.status(200).json({message: 'updated successfully'})
+    } catch (err) { 
+
+        res.status(500).json({ error: err })
+
+    }
+
+})
 
 module.exports = router;
